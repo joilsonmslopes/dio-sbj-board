6b58e3b8ed38f9d5f98eae7e9d45dd07cc470690
joilsonmslopes <joilsonmirandasl@hotmail.com>
2025-11-18 22:43:32 -0300

    fix: adjust migrations

--- src/main/resources/db/changelog/migrations/db.changelog-202511182222.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202511182222.sql
@@ -6,7 +6,6 @@ CREATE TABLE CARDS(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    `order` INT NOT NULL,
     board_column_id BIGINT NOT NULL,
     CONSTRAINT fk_boards_columns__cards_id FOREIGN KEY (board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
--- src/main/resources/db/changelog/migrations/db.changelog-202511182225.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202511182225.sql
@@ -4,10 +4,10 @@
 
 CREATE TABLE BLOCKS(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    block_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    block_reason VARCHAR(255) NOT NULL,
-    unblock_at TIMESTAMP NULL,
-    unblock_reason VARCHAR(255) NOT NULL,
+    blocked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    blocked_reason VARCHAR(255) NOT NULL,
+    unblocked_at TIMESTAMP NULL,
+    unblocked_reason VARCHAR(255) NOT NULL,
     card_id BIGINT NOT NULL,
     CONSTRAINT fk_cards__blocks_id FOREIGN KEY (card_id) REFERENCES CARDS(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
